36506c147191b90695fdc290dc6bc3cc8a76b240
noemigp0 <64096227+noemigp0@users.noreply.github.com>
2026-01-26 18:10:04 -0600

    Time: 2 ms (99.13%), Space: 47 MB (71.11%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // 0 1 2 3 4
+        //[2,7,11,15] target = 9
+        // #
+    //test to link leetHub
+        Map<Integer, Integer> numsDictionary = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int secondNumber = target - nums[i];
+            if(numsDictionary.containsKey(secondNumber)){
+                return new int[]{i, numsDictionary.get(secondNumber)};
+            }
+            numsDictionary.put(nums[i], i);
+
+
+        } 
+
+        return new int[]{};
+        
+    }
+}
