61c6cef7b7d5a340ccb65946e8eb4904d5e67b44
NOEMI GP <noemigp0@gmail.com>
2026-02-12 21:02:47 -0600

    Adding time and space complexity to problem p0643

--- src/main/java/com/arrays/slidingwindow/p0643maximumAverageSubarrayI/0643-maximum-average-subarray-i.java
+++ src/main/java/com/arrays/slidingwindow/p0643maximumAverageSubarrayI/0643-maximum-average-subarray-i.java
@@ -1,31 +1,32 @@
+package com.arrays.slidingwindow.p0643maximumAverageSubarrayI;
+
+/*
+* Big O:
+* time complexity: O(n)
+* Space complexity: O(1)*/
+
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         
-        //To keep in mind: if there is no parentesis, the operation will be perform from left to right (when + and - involved)
+
         // 0 1   2  3  4 5 
         //[1,12,-5,-6,50,3]  k=4
         //       s 
         //               e        
         //   i
 
-        // 0
-        //[-1]
-        // s
-        // e
-        //i
-
         int startWindow = 0;
         int endWindow = k - 1;
         double  sum = 0;
-        double result = Integer.MIN_VALUE;
+        double result = Double.NEGATIVE_INFINITY;//Improvements
 
         for(int i = 0; i < nums.length; i++)
         {
             sum += nums[i]; //13
 
             if( i == endWindow){
-                result = Math.max(result, sum/k);//0 , 1/4
-                sum = sum - (nums[startWindow]); //51 - 12 = 39
+                result = Math.max(result, sum/k);
+                sum = sum - (nums[startWindow]);
                 startWindow++;//1
                 endWindow++;//4
             }
