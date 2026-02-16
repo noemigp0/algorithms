e49ef66f7f40100dda76f0b2cc18b9cde8bfc226
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-02-16 12:04:25 -0600

    Time: 2 ms (90.11%), Space: 47.8 MB (76.29%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,36 +1,32 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] numsB = new int[nums.length];
+        // 0 1 2 3  4   5
+        //[1,3,12,0,0]
+        //              r
+        //              w
+
+        int reader = 0;
+        int writer = 0;
         int n = nums.length;
 
-        // 0 1 2 3 4
-        //[0,1,0,3,12]   numsB
-        //            j 
-        //[1,3,12,3,12]   nums
-        //        k
-
-        for(int i = 0; i < n ; i++){
-            numsB[i] = nums[i]; 
-        }
-
-        int j = 0;
-        int k = 0;
-        while(k < n){
-            if(j >= n){
-                nums[k] = 0;
-                k++;
-            } else if(numsB[j] != 0){
-                nums[k] = numsB[j];
-                k++;
-                j++;
-            } else{
-                j++;
-
+        while(writer < n){
+            if(reader == n){
+                nums[writer] = 0;
+                writer++;
+            } else if(nums[reader] != 0){
+                nums[writer] = nums[reader];
+                reader++;
+                writer++;
+                
+            } else {
+                reader++;
+               
             }
-            
-            
-
         }
+
+
+
+
         
     }
 }
