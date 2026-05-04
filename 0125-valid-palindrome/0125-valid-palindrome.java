fc4e0077c4ffb48fec4a1b2fb535045539f0737a
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-05-04 11:43:25 -0600

    Time: 15 ms (19.38%), Space: 46.2 MB (31.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,34 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //a man, a plan, a canal: Panama
-        //                 l                     
-        //                 r
+        String newS = s.replaceAll("[^a-zA-Z0-9]+","");
+
         int left = 0;
-        int right = s.length() -1;
+        int right = newS.length() -1;
 
-        while(left <= right){
-
-            if(!Character.isLetterOrDigit(s.charAt(left))){
-                left++;
-                continue;
-            }
-
-            if(!Character.isLetterOrDigit(s.charAt(right))){
-                right--;
-                continue;
-            }
-
-            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+        while (left <= right){
+            if(Character.toLowerCase(newS.charAt(left)) != Character.toLowerCase(newS.charAt(right)))
+            {
                 return false;
-            
-            }
 
+            }
             left++;
             right--;
 
         }
 
         return true;
-  
+        
     }
 }
