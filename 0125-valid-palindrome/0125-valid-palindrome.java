67b892705ab6e0510fe9576c6efd7f5855ce6cf9
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-05-04 13:32:49 -0600

    Time: 15 ms (19.38%), Space: 47.1 MB (13.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String newS = s.replaceAll("[^a-zA-Z0-9]+","");
 
-        int left = 0;
-        int right = newS.length() -1;
+        String cleanS = s.replaceAll("[^a-zA-Z0-9]+", "").toLowerCase();
+        String reversed = new StringBuilder(cleanS).reverse().toString();
 
-        while (left <= right){
-            if(Character.toLowerCase(newS.charAt(left)) != Character.toLowerCase(newS.charAt(right)))
-            {
-                return false;
-
-            }
-            left++;
-            right--;
-
-        }
-
-        return true;
+        return cleanS.equals(reversed);
         
     }
 }
