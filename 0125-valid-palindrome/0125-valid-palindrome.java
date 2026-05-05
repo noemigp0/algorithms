fa3702ffd56b14c48e68d3659a186a56d4dee033
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-05-05 16:53:43 -0600

    Time: 90 ms (7.72%), Space: 44.3 MB (65.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,10 +1,33 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        String cleanS = s.replaceAll("[^a-zA-Z0-9]+", "").toLowerCase();
-        String reversed = new StringBuilder(cleanS).reverse().toString();
+        //a man, a plan, a canal: panama
+        //                 l
+        //                 r
 
-        return cleanS.equals(reversed);
+        int left = 0;
+        int right = s.length() -1;
+
+        while(left <= right){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
+            }
+            
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+
+        return true;
         
     }
 }
