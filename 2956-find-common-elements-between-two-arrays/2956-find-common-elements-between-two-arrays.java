68b843f562cdb92bd4a44de290809a51ff6834c2
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-05-13 12:50:00 -0600

    Time: 7 ms (26.56%), Space: 47.2 MB (68.53%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -1,45 +1,44 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
 
-        // 0 1 2 3 4
-        //[2,3,2] 
+        // 0 1 2
+        //[2,3,2]
         //     i
         //[1,2]
-        //   j
-
-        int counter1 = 0;//2
-        int counter2 = 0;
-        Map<Integer,Integer> map1 = new HashMap<>();//{2,0}
-        Map<Integer,Integer> map2 = new HashMap<>();
-
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    counter1++;
-                    break;    
-                }
-            }
-
-        }
-
-        // 0 1 2 3 4
-        //[1,2] 
         //   i
-        //[2,3,2]
-        // j
+        //nums2Set   ->  1,2
+        //nums1Set   ->  2,3
 
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = 0; j < nums1.length; j++) {
-                if (nums2[i] == nums1[j]) {
-                    counter2++;
-                    break;
-                    
-                }
-            }
 
+        //Elements in nums1 available in nums2
+        Set<Integer> nums2Set = new HashSet<>();
+        for(Integer element: nums2){
+            nums2Set.add(element); 
         }
 
-        return new int[] { counter1, counter2 };
+        int counter1 = 0;
+        for(int i = 0; i < nums1.length; i++){
+            if(nums2Set.contains(nums1[i])){
+                counter1++;//1
+            }
+        }
 
+        Set<Integer> nums1Set = new HashSet();
+        for(Integer element: nums1){
+            nums1Set.add(element);
+        }
+
+        int counter2 = 0;
+        for(int i = 0; i < nums2.length; i++){
+            if(nums1Set.contains(nums2[i])){
+                counter2++;//1
+            }
+        }
+
+        return new int[]{counter1,counter2};
+
+
+
+        
     }
 }
