3ffd8cbe811dcb7bd30d81cae92d9f5be261d6a0
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-05-13 18:19:30 -0600

    Time: 4 ms (100%), Space: 45.8 MB (86.5%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,33 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
-
-        if (x < 0){ return false;}
-
-        String stringOfX = String.valueOf(x);
-        //012
-        //121
-        // l 
-        // r
-
-
-        int left = 0;
-        int right = stringOfX.length() - 1;
-
-        while(left <= right){
-
-            if(stringOfX.charAt(left) != stringOfX.charAt(right)){
-                return false;
-            }
-            left++;
-            right--;
-
+        if (x < 0) {
+            return false;
         }
 
-        return true;
+        //x = 121
 
+        int reversed = 0;
+        int temporalX = x;//121
 
+        while(temporalX > 0) {//121
 
+            int lastDigit = temporalX % 10;//1
+            temporalX = temporalX / 10;//0
+            reversed = reversed * 10 + lastDigit;//121
+        }
 
-        
+        return x == reversed;
+
     }
 }
