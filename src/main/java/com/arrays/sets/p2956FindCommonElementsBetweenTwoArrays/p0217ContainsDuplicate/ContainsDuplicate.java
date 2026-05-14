b0a32a16837d8e20a7ef5bb31a0db6cd4652721d
NOEMI GP <noemigp0@gmail.com>
2026-05-14 15:41:43 -0600

    Adding solution to Contains Duplicate problem

--- src/main/java/com/arrays/sets/p2956FindCommonElementsBetweenTwoArrays/p0217ContainsDuplicate/ContainsDuplicate.java
+++ src/main/java/com/arrays/sets/p2956FindCommonElementsBetweenTwoArrays/p0217ContainsDuplicate/ContainsDuplicate.java
@@ -1,4 +1,17 @@
-class Solution {
+package com.arrays.sets.p2956FindCommonElementsBetweenTwoArrays.p0217ContainsDuplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+public class ContainsDuplicate {
+
+    /*
+    * Big O:
+    * Time complexity: O(n)
+    * Space complexity: O(n)
+    * */
     public boolean containsDuplicate(int[] nums) {
         // 0 1 2 3
         //[1,2,3,1]
