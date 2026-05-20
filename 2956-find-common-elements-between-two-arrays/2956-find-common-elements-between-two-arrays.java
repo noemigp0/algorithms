37b0698c7e90c78a91352e74b8612a06c1ab3710
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-05-19 18:30:50 -0600

    Time: 7 ms (26.34%), Space: 47.3 MB (50.82%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -1,37 +1,37 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
 
-        //         0 1 2 3 4 5
-        //nums1 = [4,3,2,3,1], 
-        //                 i
-        //nums2 = [2,2,5,2,3,6]
-        //                 j
+        Set<Integer> nums1Set = new HashSet<>();//{2,3}
+        Set<Integer> nums2Set = new HashSet<>();
+        int counter1 = 0;
+        int counter2 = 0;
 
-        int nums1InNums2 = 0;//3
-        int nums2InNums1 = 0;
+        for(int num: nums1){
+            nums1Set.add(num);
+        }  
 
-        //elements in nums1 that exists in nusm2
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    nums1InNums2++;
-                    break;
-                }
+        for(int num: nums2){
+            nums2Set.add(num);
+        }  
+
+        //nums1 in nums2
+        for(int i = 0; i < nums1.length; i++){
+            if(nums2Set.contains(nums1[i])){
+                counter1++;
             }
 
         }
 
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = 0; j < nums1.length; j++) {
-                if (nums2[i] == nums1[j]) {
-                    nums2InNums1++;
-                    break;
-                }
+                //nums1 in nums2
+        for(int i = 0; i < nums2.length; i++){
+            if(nums1Set.contains(nums2[i])){
+                counter2++;
             }
 
         }
 
-        return new int[]{nums1InNums2, nums2InNums1};
-
+        return new int[]{counter1++,counter2++};
+        
+        
     }
 }
