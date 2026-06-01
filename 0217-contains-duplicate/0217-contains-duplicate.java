3facd99d3af8d5cbe39b8e0f32c8f95152196c3a
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-06-01 14:13:18 -0600

    Time: 14 ms (88.84%), Space: 97.7 MB (10.97%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,25 +1,20 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // 0 1 2 4
+
+        // 0 1 2 3
         //[1,2,3,1]
         // i
+        Set<Integer> numsSet = new HashSet<>();
 
-        //{1},{},{}
-        
 
-        Set<Integer> hashSetNums = new HashSet<>();
-
-        for(int i=0; i < nums.length; i++){
-            if(hashSetNums.contains(nums[i])){
+        for(Integer number: nums){
+            if(numsSet.contains(number)){
                 return true;
             }
-            hashSetNums.add(nums[i]);
-
+            numsSet.add(number);
         }
 
         return false;
-
-        
         
     }
 }
