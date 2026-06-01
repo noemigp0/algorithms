bd690e3aa05c3798418b7babaf9885c83128b8d9
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-06-01 14:34:48 -0600

    Time: 7 ms (28.14%), Space: 47.6 MB (15.68%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -2,35 +2,37 @@ class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
 
         //nums1 in nums2
- 
-        int counter1  = 0;//3
+        Set<Integer> nums1Set = new HashSet<>();
+        Set<Integer> nums2Set = new HashSet<>();
+        int counter1 = 0;
         int counter2 = 0;
+        //nums1 = [4,3,2,3,1], nums2Set {2,5,3,6]}
 
-        //         0 1 2 3 4
-        //nums1 = [4,3,2,3,1], 
-        //             j
         //nums2 = [2,2,5,2,3,6]
-        //         i
 
-        for(int i = 0; i < nums1.length; i++){
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    counter1++;
-                    break;
-                }
-            }
+        for (Integer number : nums1) {
+            nums1Set.add(number);
         }
 
-        for(int i = 0; i < nums2.length; i++){
-            for(int j = 0; j < nums1.length; j++){
-                if(nums1[j] == nums2[i]){
-                    counter2++;
-                    break;
-                }
+        for (Integer number : nums2) {
+            nums2Set.add(number);
+        }
+
+        for (Integer number : nums1) {
+            if (nums2Set.contains(number)) {
+                counter1++;//3
             }
+
+        }
+
+        for (Integer number : nums2) {
+            if (nums1Set.contains(number)) {
+                counter2++;//3
+            }
+
         }
 
         return new int[]{counter1,counter2};
-        
+
     }
 }
