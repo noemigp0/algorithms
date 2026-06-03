2f2ea26a182a838b2b29f723444efb0a163a09fb
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-06-03 15:32:40 -0600

    Time: 2 ms (6.89%), Space: 43.1 MB (25.18%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,55 +1,53 @@
 class Solution {
     public boolean check(int[] nums) {
-
-        //notes: Do you really need aother array
-        // do you really need to keep rotating if the minimum number is already at position 0
-        //if the minimum number is already at position 0 do you need to keep validating the order?
-        if (nums.length == 1) {
+        if (nums.length == 0 || isSorted(nums)) {
             return true;
         }
-
-        int[] auxArray = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            auxArray[i] = nums[i];
+        //creating copy
+        int[] numsSorted = new int[nums.length];
+        for (int i = 0; i < numsSorted.length; i++) {
+            numsSorted[i] = nums[i];
         }
-        //            0 1 2 3 
-        //nums     = [1,2,3]
+        //        0 1 2 3 4
+        //nums = [2,1,3,4]
+        //            i
+        //       [1,3,4,2]
 
-        //auxArray = [1,2,3]    
-        //                i
+
 
         int rotations = 0;
-        int len = auxArray.length - 1; //3
+        int len = numsSorted.length;
 
-        while (rotations < auxArray.length) {// rotations 4
+        while (rotations < len) {
+            int auxCopy = numsSorted[0];//2
 
-            int breaks = 0;
-            for (int i = 1; i < auxArray.length; i++) {
-                if (auxArray[i - 1] > auxArray[i]) {
-                    breaks++;//1
-                    break;
-                }
-
+            for (int i = 0; i < len - 1; i++) {
+                numsSorted[i] = numsSorted[i + 1];
             }
+            numsSorted[len - 1] = auxCopy;
 
-            if (breaks == 0) {
+            if (isSorted(numsSorted)) {
                 return true;
             }
-
-            int firstNumber = auxArray[0];//4
-            for (int i = 1; i < auxArray.length; i++) {
-
-                auxArray[i - 1] = auxArray[i];
-                if (i == len) {
-                    auxArray[i] = firstNumber;
-                }
-
-            }
-
             rotations++;
-
         }
 
         return false;
+
+    }
+
+    // 0 1 2 3 
+    //[1,3,4,2]
+    //       i
+
+    public static boolean isSorted(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+
+        }
+        return true;
+
     }
 }
