9c6d21776e0e84224c46e72d705a42dcec414192
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-06-08 18:52:16 -0600

    Time: 0 ms (100%), Space: 43.3 MB (10.93%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,21 @@
 class Solution {
     public boolean check(int[] nums) {
 
+
         // 0 1 2 3 4
         //[2,1,3,4]
-        //       i   
+        //       i
+        // +
 
-        //1.-Solo puede haber un "break"
-        //2.-ya que compararlo de manera circular
-
-        int n = nums.length - 1;
         int breaks = 0;
-
-        for (int i = 1; i <= n; i++) {
-
-            if (nums[i] < nums[i - 1]) {
-                breaks++;//1
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > nums[(i+1)%nums.length]){
+                breaks++;//2
             }
 
         }
 
-        if (nums[n] > nums[0]) {
-            breaks++;
-        }
-
-        return breaks <= 1;
-
+        return breaks<=1;
+        
     }
 }
