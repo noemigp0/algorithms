56081479403256a63d75da352bab4b7dbc1c7f67
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-07-06 13:18:55 -0600

    Time: 0 ms (100%), Space: 43.6 MB (76.48%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,29 +1,33 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        //         0 1 2 3 4 5
-        //nums1 = [1,2,2,3,5,6], m = 3, 
-        //           i
-        //           1
-        //nums2 = [2,5,6], n = 3 
-        //       2
- 
-        int n1 = m -1;
-        int n2 = n -1;
+        //          0 1 2 3 4 5
+        // nums1 = [1,2,3,0,0,6], m = 3, 
+        //              1
+        //                  w
+        // nums2 = [2,5,6], n = 3
+        //            2
 
-        for(int i = nums1.length -1; i >= 0; i--){
-            if(n2 < 0){
-                break;
-            }
-            if(n1 >= 0 && nums1[n1] > nums2[n2]){
-                nums1[i] = nums1[n1];
-                n1--;
+      
+
+
+        int reader1 = m - 1; //2
+        int reader2 = n - 1;//2
+        int writter = m + n - 1;//5
+
+        while (writter >= 0 && reader2 >=0) {
+
+            if (reader1 >= 0 && nums1[reader1] > nums2[reader2]) {
+                nums1[writter] = nums1[reader1];
+                reader1--;
             } else {
-                nums1[i] = nums2[n2];
-                n2--;
+                nums1[writter] = nums2[reader2];
+                reader2--;
             }
+
+            writter--;
 
         }
-        
+
     }
 }
