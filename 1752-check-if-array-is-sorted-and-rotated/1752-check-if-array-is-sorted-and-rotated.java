a3251cd56880b59016bbbbfa567098f39f945df6
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-07-12 17:19:02 -0600

    Time: 2 ms (6.8%), Space: 43.1 MB (43.71%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,26 +1,39 @@
 class Solution {
     public boolean check(int[] nums) {
+        // 0 1 2 3 4
+        //[2,1,3,4]
+        // i
 
-        //        0 1 2 3 4
-        //nums = [5,1,5,1]
-        //              i  
-        int n = nums.length;
-        int breaks = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] > nums[(i+1)%n]){ 
-                breaks++;//2
+        //rotar y verificar si esta ordenado
 
-                if(breaks > 1){
-                    return false;
-                }
+        int n = nums.length; //4
+        int rotations = 0;
+
+        boolean isSorted = isSorted(nums);
+
+        while (rotations <= n) {//2 && true
+            int aux = nums[0];//5
+            for (int i = 0; i < n - 1; i++) {
+                nums[i] = nums[i + 1];
+            }
+            nums[n - 1] = aux;
+            rotations++;//3
+            if(isSorted(nums)){
+                return true;
             }
         }
 
+        return false;
 
-        return breaks <= 1;
+    }
 
+    public boolean isSorted(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
 
-
-        
     }
 }
