142f2726f1a10a2dbfd775901777e3985eeee1f3
Noemi Gutierrez <64096227+noemigp0@users.noreply.github.com>
2026-07-12 21:27:27 -0600

    Time: 0 ms (100%), Space: 44.5 MB (55.58%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,27 +1,26 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
 
-     //    0 1
-    //0// [1,5]
-    //1// [7,3]
-    //2// [3,5]#
-    //     
+        //   0 1
+        //0 [1,5]  {0,0},{0,1}
+        //1 [7,3]  {1,0},{1,1}
+        //2 [3,5]  {2,0},{2,1}
 
-    
-    int maxWealth = 0;
-    for(int row = 0; row < accounts.length; row++){
-        int wealthPerCustomer = 0;
-        for(int col = 0; col < accounts[row].length; col++){
+        
+        int maxWealth = 0;
 
-            wealthPerCustomer += accounts[row][col];//
-            maxWealth = Math.max(wealthPerCustomer, maxWealth);//10
+        for(int row = 0; row < accounts.length; row++){//1
+        int wealth = 0;
+            for(int col = 0; col < accounts[row].length; col++){//1
+
+                wealth += accounts[row][col];//10
+                
+            }
+                maxWealth = Math.max(maxWealth, wealth);//10
+
         }
 
-    }
-
-    return maxWealth;
-
-
+        return maxWealth;
         
     }
 }
